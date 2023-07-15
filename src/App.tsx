df30d192fcec50ed8025537adf77a26eba196881
Jose Miguel <josemiguelmedina0210@gmail.com>
2023-07-15 19:03:44 +0800

    adding the component codes and finishing touches

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
+import ScoreDisplay from "./components/scores";
+import Questions from "./components/questions";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="canvas">
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/scores" element={<ScoreDisplay />} />
+        </Routes>
+      </main>
     </div>
   );
 }
